b068ba913da146e02d8b663d931c7b8d27821e43
Daniel <daniel@ilearnedthisthehardway.com>
2017-08-06 13:46:41 -0400

    adding menu script and modified Dockerfile to use it

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,5 @@ RUN apt-get update && apt-get install -y \
 && rm -rf /var/lib/apt/lists/*
 RUN git clone https://github.com/NextThingCo/CHIP-SDK.git
 RUN export logname=root && /bin/bash CHIP-SDK/setup*
-RUN git clone https://github.com/techgeeksid/chip-Flash-tool.git
-RUN cd /chip-Flash-tool && git pull && chmod +x /chip-Flash-tool/chip.sh
-ENTRYPOINT ["/chip-Flash-tool/chip.sh"]
+RUN git clone https://github.com/dswhitley/CHIPinteractive.git
+ENTRYPOINT ["/bin/bash /CHIPinteractive/menu.sh"]
---
+++ menu.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+#:Title	: menu
+#!Date	: 2017-08-06
+#!Author: dswhitley
+#!Ref   : https://docs.getchip.com/chip.html#customize-buildroot
+while true; do
+  clear;
+  echo "                 #  #  #               ";
+  echo "               #########               ";
+  echo "             ###       ###             ";
+  echo "               # {#}   #               ";
+  echo "             ###  '%######             ";
+  echo "               #       #               ";
+  echo "             ###       ###             ";
+  echo "               ########                ";
+  echo "                #  #  #                ";
+  echo "=======================================";
+  echo "Menu...                                ";
+  echo "=======================================";
+  echo "Enter 0 to revert back to default      ";
+  echo "Enter 1 to customize buildroot         ";
+  echo "Enter 2 to customize Linux             ";
+  echo "Enter 3 to stop this container         ";
+  echo "                                       ";
+  echo "=======================================";
+  echo "Enter option Number:                   ";
+
+  read answer;
+  case "$answer" in
+    0) clear;
+       echo "...ensure that your C.H.I.P. board is jumpered for 'fel' mode as noted in the documentation";
+       sleep 5;
+       cd /CHIP-tools;
+       ./chip-update-firmware.sh -s
+       clear;
+       echo "Your C.H.I.P. has been flashed with the NTC firmware.";
+       echo "You can remove the jumper and boot it normally...";
+       echo "Or set up another one.";
+       read -p "= Press any key to continue..." -n1 -s;;
+    1) clear;
+       echo "You will be presented with a TUI in which to customize your build:";
+       sleep 5;
+       cd /CHIP-buildroot;
+       make chip_defconfig;
+       make nconfig;
+       clear;
+       echo "This next step will take a long time, please be patient.";
+       read -p "= Press any key to continue..." -n1 -s;
+       make;
+       read -p "= Press any key to continue..." -n1 -s;;
+    2) echo "and this is linux-config";
+       read -p "= Press any key to continue..." -n1 -s;;
+    3) echo "this step will use the custom rootfs to update the C.H.I.P.";
+       cd /CHIP-tools;
+       echo "Create buildroot's intermediate image files";
+       # I should make outputdir a volume in my container...
+       ./chip-create-nand-images.sh ../CHIP-Buildroot/output/build/uboot* ../CHIP-Buildroot/output/images/rootfs.tar outputdir;
+       echo "...ensure that your C.H.I.P. board is jumpered for 'fel' mode as noted in the documentation";
+       sleep 5;
+       ./chip-update-firmware.sh -L outputdir/;
+       echo "Your C.H.I.P. has been flashed with customized firmware.";    
+       read -p "= Press any key to continue..." -n1 -s;;
+    4) echo "you wanted to exit, here we go"; exit
+  esac
+  echo "Enter new option:";
+done;
