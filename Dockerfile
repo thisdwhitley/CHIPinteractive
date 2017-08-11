8afaaeebded56067607a3d3d11520e9576a83361
Daniel Whitley <30498158+dswhitley@users.noreply.github.com>
2017-08-11 16:13:50 -0400

    Develop (#8)
    * added some more packages to image
    
    * using the 'official' CHIP-SDK repository for config
    
    * there are lots of warnings, but image built after adding sudo and make packages
    
    * adding wget to the Dockerfile because the flash won't work without it
    
    * attempting to set logname as environment var before running first script
    
    * adding NOTE about build time
    
    * adding menu script and modified Dockerfile to use it
    
    * added bc package needed for buildroot commands
    
    * removed the failed attempt to export global variable and combined RUN commands
    
    * using the develop branch of CHIPinteractive in image
    
    * updating menu with custom flash option
    
    * adding gitignore so the downloaded files are stored in git
    
    * adding .dockerignore so the downloaded files are ignored when building image
    
    * fixed B to b
    
    * working around the disappearance of atenart
    
    * updating for wrong directory when specifying local
    
    * added wording about skipping the 'make linux-nconfig' part

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,16 @@
 FROM ubuntu:16.04
 WORKDIR /
 RUN apt-get update && apt-get install -y \
+    bc \
     curl \
     git \
     make \
-    pkg-config \
     sudo \
     usbutils \
     wget \
 && rm -rf /var/lib/apt/lists/*
-RUN git clone https://github.com/techgeeksid/chip-Flash-tool.git
-RUN cd /chip-Flash-tool && git pull && chmod +x /chip-Flash-tool/chip.sh
-ENTRYPOINT ["/chip-Flash-tool/chip.sh"]
+RUN git clone https://github.com/NextThingCo/CHIP-SDK.git \
+    && /bin/bash /CHIP-SDK/setup*
+RUN git clone -b develop https://github.com/dswhitley/CHIPinteractive.git \
+    && chmod +x /CHIPinteractive/menu.sh
+ENTRYPOINT ["/CHIPinteractive/menu.sh"]
---
+++ menu.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+#:Title	: menu
+#!Date	: 2017-08-06
+#!Author: dswhitley
+#!Ref   : https://docs.getchip.com/chip.html#customize-buildroot
+while true; do
+  clear;
+  echo "                 #  #  #               ";
+  echo "               #########               ";
+  echo "             ###       ###             ";
+  echo "               # {#}   #               ";
+  echo "             ###  '%######             ";
+  echo "               #       #               ";
+  echo "             ###       ###             ";
+  echo "               ########                ";
+  echo "                #  #  #                ";
+  echo "=======================================";
+  echo "Menu...                                ";
+  echo "=======================================";
+  echo "Enter 0 to revert back to default      ";
+  echo "Enter 1 to customize buildroot         ";
+  echo "XXXEnter 2 to customize LinuxXXX       ";
+  echo "Enter 3 to flash with customizations   ";
+  echo "Enter 4 to stop this container         ";
+  echo "                                       ";
+  echo "=======================================";
+  echo "Enter option Number:                   ";
+
+  read answer;
+  case "$answer" in
+    0) clear;
+       echo "...ensure that your C.H.I.P. board is jumpered for 'fel' mode as noted in the documentation";
+       sleep 5;
+       cd /CHIP-tools;
+       ./chip-update-firmware.sh -s
+       clear;
+       echo "Your C.H.I.P. has been flashed with the NTC firmware.";
+       echo "You can remove the jumper and boot it normally...";
+       echo "Or set up another one.";
+       read -p "= Press any key to continue..." -n1 -s;;
+    1) clear;
+       echo "You will be presented with a TUI in which to customize your build:";
+       sleep 5;
+       cd /CHIP-buildroot;
+       make chip_defconfig;
+       make nconfig;
+       clear;
+       echo "This next step will take a long time, please be patient.";
+       read -p "= Press any key to continue..." -n1 -s;
+       # this appears to be a bug, but remove atenart:
+       sed -i 's/atenart/nextthingco/' /CHIP-buildroot/package/dtc-overlay/dtc-overlay.mk;
+       make;
+       read -p "= Press any key to continue..." -n1 -s;;
+    2) echo "I'm sorry, this has been abandoned but should have done the 'make linux-config' part...";
+       read -p "= Press any key to continue..." -n1 -s;;
+    3) echo "this step will use the custom rootfs to update the C.H.I.P.";
+       cd /CHIP-tools;
+       echo "Create buildroot's intermediate image files";
+       # I should make outputdir a volume in my container...
+       ./chip-create-nand-images.sh ../CHIP-buildroot/output/build/uboot* ../CHIP-buildroot/output/images/rootfs.tar outputdir;
+       echo "...ensure that your C.H.I.P. board is jumpered for 'fel' mode as noted in the documentation";
+       sleep 5;
+       # it looks like a guess is made for location of directory, remove it:
+       sed -i 's/\.\.\/\.\.\/\$LOCALDIR/\$LOCALDIR/' /CHIP-tools/chip-update-firmware.sh
+       ./chip-update-firmware.sh -L outputdir/;
+       echo "Your C.H.I.P. has been flashed with customized firmware.";    
+       read -p "= Press any key to continue..." -n1 -s;;
+    4) echo "you wanted to exit, here we go"; exit
+  esac
+  echo "Enter new option:";
+done;
